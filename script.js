4f3ac361689fc43859d08845228bd8f6f882b6b7
Carolyn Lee <lcarolyn@stanford.edu>
2024-07-01 09:56:49 -0700

    updated styles/format

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar-placeholder").innerHTML = data;
+        });
+});
